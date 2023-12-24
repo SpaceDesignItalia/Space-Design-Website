5122eeda3a01eed529e7c5e797d0ca2766405e30
Andrea Braia <andrix.braia@gmail.com>
2023-12-24 16:03:07 +0100

    Stable develop

---
+++ .vite/deps/@mui_icons-material_Web.js
@@ -0,0 +1,30 @@
+"use client";
+import {
+  require_createSvgIcon,
+  require_interopRequireDefault
+} from "./chunk-WTQD6RDS.js";
+import {
+  require_jsx_runtime
+} from "./chunk-VMXF7Q6U.js";
+import {
+  __commonJS
+} from "./chunk-W4VDMLRG.js";
+
+// node_modules/@mui/icons-material/Web.js
+var require_Web = __commonJS({
+  "node_modules/@mui/icons-material/Web.js"(exports) {
+    var _interopRequireDefault = require_interopRequireDefault();
+    Object.defineProperty(exports, "__esModule", {
+      value: true
+    });
+    exports.default = void 0;
+    var _createSvgIcon = _interopRequireDefault(require_createSvgIcon());
+    var _jsxRuntime = require_jsx_runtime();
+    var _default = (0, _createSvgIcon.default)((0, _jsxRuntime.jsx)("path", {
+      d: "M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm-5 14H4v-4h11v4zm0-5H4V9h11v4zm5 5h-4V9h4v9z"
+    }), "Web");
+    exports.default = _default;
+  }
+});
+export default require_Web();
+//# sourceMappingURL=@mui_icons-material_Web.js.map
