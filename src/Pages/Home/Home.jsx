4b1caacf8e6e650c94a1b6fbb84e8b03bd033dee
Andrea Braia <andrix.braia@gmail.com>
2024-01-14 12:08:47 +0100

    Added Service section

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -5,15 +5,17 @@ import GetInTouch from "./GetInTouch";
 import Stats from "./Stats";
 import AboutUs from "./AboutUs";
 import TechStack from "./TechStack";
+import Services from "./Services";
 
 export default function Home() {
   return (
     <div>
       <Hero />
-      {/* <Partners /> */}
+      <Partners />
+      <Services />
+      <TechStack />
       <AboutUs />
       <Stats />
-      <TechStack />
       <GetInTouch />
     </div>
   );
