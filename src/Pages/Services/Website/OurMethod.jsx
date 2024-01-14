9e7874163106b23c93e3605e5d1d9746e1b13c15
Francesco Roviello <frarovi04@gmail.com>
2024-01-14 19:48:40 +0100

    OurMethod section edit

--- src/Pages/Services/Website/OurMethod.jsx
+++ src/Pages/Services/Website/OurMethod.jsx
@@ -1,123 +1,54 @@
 import React from "react";
+import GroupsIcon from "@mui/icons-material/Groups";
+import AutoFixHighIcon from "@mui/icons-material/AutoFixHigh";
+import TimerIcon from "@mui/icons-material/Timer";
 
-export default function OurMethod() {
+const cards = [
+  {
+    name: "Design accattivanti",
+    description:
+      "Ogni sito web che realizziamo è concepito con un occhio attento al dettaglio estetico, combinando estetica moderna con funzionalità intuitiva.",
+    icon: AutoFixHighIcon,
+  },
+  {
+    name: "Velocità di sviluppo",
+    description:
+      "Grazie a un processo di sviluppo efficiente e a un team altamente competente, siamo in grado di consegnare siti web di alta qualità in tempi record.",
+    icon: TimerIcon,
+  },
+  {
+    name: "Team dedicato",
+    description:
+      "Dalla fase iniziale di pianificazione fino alla pubblicazione e oltre, avrete un team che si impegna a capire la vostra visione e a trasformarla in realtà.",
+    icon: GroupsIcon,
+  },
+];
+
+export default function WebsiteCards() {
   return (
-    <div class="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
-      <div class="mx-auto max-w-2xl lg:text-center">
-        <h2 class="text-3xl font-bold sm:text-4xl">
-          Un viaggio digitale strutturato
-        </h2>
-
-        <p class="mt-6 text-lg leading-8 text-gray-800">
-          Nel nostro approccio di sviluppo, uniamo creatività e tecnologia,
-          creando soluzioni che sfidano le aspettative. Attraverso analisi
-          approfondite e innovazione continua, costruiamo progetti robusti e
-          rivoluzionari. La nostra filosofia si basa sulla collaborazione
-          aperta, creando un partenariato con i clienti per un successo
-          condiviso.
-        </p>
-      </div>
-
-      <div class="mt-8 grid grid-cols-1 gap-8 md:mt-16 md:grid-cols-2 md:gap-12 lg:grid-cols-3">
-        <div class="flex items-start gap-4">
-          <span class=" w-12 h-12 flex justify-center items-center text-white text-lg font-bold shrink-0 rounded-lg bg-primary p-4">
-            1
-          </span>
-
-          <div>
-            <h2 class="text-lg font-bold">Chiamata conoscitiva</h2>
-
-            <p class="mt-1 text-base text-gray-800">
-              Iniziamo con una chiamata conoscitiva, un momento dedicato a
-              comprendere profondamente le tue esigenze. Ascoltiamo la tua
-              visione, analizziamo le sfide e delineiamo obiettivi chiari per il
-              tuo progetto digitale.
-            </p>
-          </div>
+    <div className="py-24 sm:py-32">
+      <div className="text-center mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-7xl lg:mx-0">
+          <h2 class="text-3xl font-bold sm:text-4xl">Perchè scegliere noi</h2>
+          <p className="mt-6 text-lg leading-8">
+            Con noi, il vostro sito web non sarà solo un luogo virtuale, ma
+            un'esperienza che lascia un'impronta duratura sulla mente degli
+            utenti.
+          </p>
         </div>
-
-        <div class="flex items-start gap-4">
-          <span class=" w-12 h-12 flex justify-center items-center text-white text-lg font-bold shrink-0 rounded-lg bg-primary p-4">
-            2
-          </span>
-
-          <div>
-            <h2 class="text-lg font-bold">Prototipazione</h2>
-
-            <p class="mt-1 text-base text-gray-800">
-              Dopo la chiamata, entriamo nella fase di prototipazione, dove
-              trasformiamo le idee in una bozza tangibile del progetto. Questo
-              processo ci consente di collaborare strettamente con te,
-              assicurando che il risultato finale rispecchi pienamente le tue
-              aspettative.
-            </p>
-          </div>
-        </div>
-
-        <div class="flex items-start gap-4">
-          <span class=" w-12 h-12 flex justify-center items-center text-white text-lg font-bold shrink-0 rounded-lg bg-primary p-4">
-            3
-          </span>
-
-          <div>
-            <h2 class="text-lg font-bold">Sviluppo</h2>
-
-            <p class="mt-1 text-base text-gray-800">
-              Con la tua approvazione del prototipo, entriamo nella fase
-              cruciale dello Sviluppo. Qui, il nostro team di esperti traduce il
-              design e le funzionalità desiderate in codice, creando un prodotto
-              digitale che rispecchia la tua identità e raggiunge gli obiettivi
-              stabiliti.
-            </p>
-          </div>
-        </div>
-
-        <div class="flex items-start gap-4">
-          <span class=" w-12 h-12 flex justify-center items-center text-white text-lg font-bold shrink-0 rounded-lg bg-primary p-4">
-            4
-          </span>
-
-          <div>
-            <h2 class="text-lg font-bold">Testing</h2>
-
-            <p class="mt-1 text-base text-gray-800">
-              Segue una fase di testing, in cui mettiamo alla prova ogni
-              dettaglio del progetto. Verifichiamo la stabilità, l'usabilità e
-              la sicurezza per garantire un'esperienza utente impeccabile.
-            </p>
-          </div>
-        </div>
-
-        <div class="flex items-start gap-4">
-          <span class=" w-12 h-12 flex justify-center items-center text-white text-lg font-bold shrink-0 rounded-lg bg-primary p-4">
-            5
-          </span>
-
-          <div>
-            <h2 class="text-lg font-bold">Deploy</h2>
-
-            <p class="mt-1 text-base text-gray-800">
-              Giungiamo al culmine del nostro processo con la fase di Deploy, in
-              cui il tuo progetto prende vita online. Qui, trasformiamo la
-              soluzione in realtà digitale.
-            </p>
-          </div>
-        </div>
-
-        <div class="flex items-start gap-4">
-          <span class=" w-12 h-12 flex justify-center items-center text-white text-lg font-bold shrink-0 rounded-lg bg-primary p-4">
-            6
-          </span>
-
-          <div>
-            <h2 class="text-lg font-bold">Supporto Tecnico</h2>
-
-            <p class="mt-1 text-base text-gray-800">
-              Il nostro impegno prosegue oltre il lancio iniziale. Forniamo un
-              supporto tecnico continuo, un affidabile alleato che accompagna
-              costantemente la tua esperienza digitale.
-            </p>
-          </div>
+        <div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-6 sm:mt-20 lg:mx-0 lg:max-w-none lg:grid-cols-3 lg:gap-8">
+          {cards.map((card) => (
+            <div
+              key={card.name}
+              className="text-left flex gap-x-4 rounded-xl bg-black/5 p-6 ring-1 ring-inset ring-black/10"
+            >
+              <card.icon className="h-7 w-5 flex-none" aria-hidden="true" />
+              <div className="text-base leading-7">
+                <h3 className="font-semibold text-black">{card.name}</h3>
+                <p className="mt-2 ">{card.description}</p>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
